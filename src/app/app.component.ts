85f3b9f8273b7225981230553c20a2468109ff9b
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-10-07 10:35:10 +0700

    create add wish and filter wish functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/model/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
+  template: `{{ wish }}`,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'wishlist';
+
+  items: WishItem[] = [
+    new WishItem('Album HHW'),
+    new WishItem('Nesoberi Kokoron', true),
+    new WishItem('Jake Kokoro'),
+  ];
+
+  newWishText = '';
+  addNewWish() {
+    if (this.newWishText) {
+      this.items.push(new WishItem(this.newWishText));
+      this.newWishText = '';
+    }
+  }
+
+  listFilter: String = '0';
+  get visibleItems(): WishItem[] {
+    switch (this.listFilter) {
+      case '1':
+        return this.items.filter((item) => !item.isComplete);
+
+      case '2':
+        return this.items.filter((item) => item.isComplete);
+
+      default:
+        return this.items;
+    }
+  }
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }
